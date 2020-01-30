55901f07b96e9ddbba9899a9fa49054493113fea
AlecVidamo <55145798+AlecVidamo@users.noreply.github.com>
2020-01-30 17:29:33 -0500

    Added score text UI and updated player controller script to allow the score to update as we play

--- Assets/Scripts/Player_Controller.cs
+++ Assets/Scripts/Player_Controller.cs
@@ -1,15 +1,21 @@
 using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class Player_Controller : MonoBehaviour {
 
     public float speed;
+    public Text scoreText;
+    public Text winText;
 
     private Rigidbody rb;
+    private int score;
 
     void Start ()
     {
         rb = GetComponent<Rigidbody>();
+        score = 0;
+        SetScoreText ();
     }
 
     void FixedUpdate ()
@@ -26,6 +32,17 @@ public class Player_Controller : MonoBehaviour {
         if (other.gameObject.CompareTag("Pick Up"))
         {
             other.gameObject.SetActive(false);
+            score = score + 1;
+            SetScoreText ();
+        }
+    }
+    
+    void SetScoreText()
+    {
+        scoreText.text = "Score: " + score.ToString();
+        if (score >= 14)
+        {
+            winText.text = "You Win!";
         }
     }
 }
